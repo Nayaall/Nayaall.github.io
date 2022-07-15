3077c18d7e256511b7409c3c1d7f4fd7f2323a5b
Nayaall <108759848+Nayaall@users.noreply.github.com>
2022-07-15 17:47:26 +0200

    Shortened spin statement to improve efficiency

--- Lucky_Wheel_scripts.js
+++ Lucky_Wheel_scripts.js
@@ -1,9 +1,6 @@
 function spin() {
-for (let a = 0; a < 361; a++) {
-  document.getElementById("").transform = "rotate(1deg)"
-}
 let randnum = Math.floor(Math.random() * 360);
-for (let b = 0; b < randnum; b++) {
+for (let a = 0; a < 361 + randnum; a++) {
   document.getElementById("").transform = "rotate(1deg)"
 }
 }
