f01471bf202fc1d92ce8d178bce75734e6a84e25
Nayaall <108759848+Nayaall@users.noreply.github.com>
2022-07-15 17:40:39 +0200

    Added lucky wheel rotation to spin function

--- Lucky_Wheel_scripts.js
+++ Lucky_Wheel_scripts.js
@@ -1,3 +1,9 @@
 function spin() {
-
+for (let a = 0; a < 361; a++) {
+  document.getElementById("").transform = "rotate(1deg)"
+}
+let randnum = Math.floor(Math.random() * 360);
+for (let b = 0; b < randnum; b++) {
+  document.getElementById("").transform = "rotate(1deg)"
+}
 }
