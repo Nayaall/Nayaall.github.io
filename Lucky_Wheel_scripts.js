e7337f20e8fd165c0cc7c0760f6bb572b9b17c2e
Nayaall <108759848+Nayaall@users.noreply.github.com>
2022-07-17 13:45:09 +0100

    Added slowing down of the wheel

--- Lucky_Wheel_scripts.js
+++ Lucky_Wheel_scripts.js
@@ -1,6 +1,20 @@
-function spin() {
-let randnum = Math.floor(Math.random() * 360);
-for (let a = 0; a < 361 + randnum; a++) {
-  document.getElementById("").transform = "rotate(1deg)"
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
 }
+function spin() {
+  let randnum = Math.floor(Math.random() * 360);
+  for (let a = 0; a < 361 + randnum; a++) {
+    document.getElementById("").transform = "rotate(1deg)"
+    if (a<150) {
+      sleep(0.1);
+    }else if (a>150 && a<365) {
+      sleep(0.3);
+    }else if (a>365) {
+      sleep((a--361)/10);
+    } 
+  }
 }
